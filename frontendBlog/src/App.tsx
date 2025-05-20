2c8b0796933c9dd431657e416ce5b53a7fc5cb7e
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-05-20 17:17:37 -0300

    feat: route protection working using PrivateRoute component

--- frontendBlog/src/App.tsx
+++ frontendBlog/src/App.tsx
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
+import { PrivateRoute } from './components/PrivateRoute';
 import { HomePage } from './pages/HomePage';
+
 import './App.css'
 
 
@@ -9,8 +11,15 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-      <Route path="/home" element={<HomePage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path="/home"
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
