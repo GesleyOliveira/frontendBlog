d1da67b1c54d64cff16526269451f901d7d2e6a9
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-05-20 17:22:18 -0300

    feat: create LoadingPage

---
+++ frontendBlog/src/pages/LoadingPage.tsx
@@ -0,0 +1,10 @@
+import './loading.css';
+
+export function LoadingPage() {
+  return (
+    <div className="loading-container">
+      <h1 className="logo">M.</h1>
+      <p className="subtitle">Conteúdo que inspira</p>
+    </div>
+  );
+}
