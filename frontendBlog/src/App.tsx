735f7afee4269bbe1284112908102e3020b3fd24
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-05-20 17:25:04 -0300

    feat: implement LoadingPage with redirect to login after delay

--- frontendBlog/src/App.tsx
+++ frontendBlog/src/App.tsx
@@ -1,16 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from './pages/LoginPage';
+import { HomePage } from './pages/HomePage';
 import { RegisterPage } from './pages/RegisterPage';
 import { PrivateRoute } from './components/PrivateRoute';
-import { HomePage } from './pages/HomePage';
-
-import './App.css'
-
+import { LoadingPage } from './pages/LoadingPage';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<LoadingPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route
         path="/home"
@@ -23,4 +22,3 @@ export default function App() {
     </Routes>
   );
 }
-
--- frontendBlog/src/pages/LoadingPage.tsx
+++ frontendBlog/src/pages/LoadingPage.tsx
@@ -1,6 +1,18 @@
-import './loading.css';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/loading.css';
 
 export function LoadingPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 2000); // 2 segundos de delay
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div className="loading-container">
       <h1 className="logo">M.</h1>
